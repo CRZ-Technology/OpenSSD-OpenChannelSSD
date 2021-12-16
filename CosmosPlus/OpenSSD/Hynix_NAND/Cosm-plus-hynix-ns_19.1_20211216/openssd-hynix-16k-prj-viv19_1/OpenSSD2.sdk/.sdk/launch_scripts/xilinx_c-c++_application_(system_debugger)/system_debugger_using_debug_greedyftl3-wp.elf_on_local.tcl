dcd3a2a2d48da8e322d51c14bb49447c65d96e56
CRZ-Technology <94947807+CRZ-Technology@users.noreply.github.com>
2021-12-16 17:18:14 +0900

    Support multiple namespace

---
+++ CosmosPlus/OpenSSD/Hynix_NAND/Cosm-plus-hynix-ns_19.1_20211216/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_greedyftl3-wp.elf_on_local.tcl
@@ -0,0 +1,16 @@
+connect -url tcp:127.0.0.1:3121
+source D:/works_in_swiss-cosmos-resynth/projects-viv19_1/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/OpenSSD2_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251845683"} -index 0
+loadhw -hw D:/works_in_swiss-cosmos-resynth/projects-viv19_1/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/OpenSSD2_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251845683"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251845683"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251845683"} -index 0
+dow D:/works_in_swiss-cosmos-resynth/projects-viv19_1/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/GreedyFTL3-wp/Debug/GreedyFTL3-wp.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251845683"} -index 0
+con
---
+++ CosmosPlus/OpenSSD/Hynix_NAND/Cosm-plus-hynix-ns_19.1_20211216/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/fsbl/Debug/src/ps7_init.d
@@ -0,0 +1,32 @@
+src/ps7_init.o src/ps7_init.o: \
+ D:/sources/OpenSSD2-8C8W-16K_19.1_20200728/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/OpenSSD2_hw_platform_0/ps7_init.c \
+ D:/sources/OpenSSD2-8C8W-16K_19.1_20200728/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/OpenSSD2_hw_platform_0/ps7_init.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+D:/sources/OpenSSD2-8C8W-16K_19.1_20200728/openssd-hynix-16k-prj-viv19_1/OpenSSD2.sdk/OpenSSD2_hw_platform_0/ps7_init.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
