563f7b58d00fc6c34f4fcc802f8473244c03d71a
CRZ-Technology <94947807+CRZ-Technology@users.noreply.github.com>
2023-01-27 16:37:43 +0900

    Increased address space of NVMe IP to 40bit.

---
+++ DaisyPlus/NVMe/MIG/LPDDR4_B/SI5391B/DaisyPlus_NVMe_MIG_19.1_LPDDR4_B_SI5391B/OpenSSD/OpenSSD.sdk/nvme/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/main.c 
+
+OBJS += \
+./src/main.o 
+
+C_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v8 gcc compiler'
+	aarch64-none-elf-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../nvme_bsp/psu_cortexa53_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
