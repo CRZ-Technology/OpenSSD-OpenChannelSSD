b80f2a384c83ec12442cfa157570affbb26e3b4b
CRZ-Technology <94947807+CRZ-Technology@users.noreply.github.com>
2024-08-19 10:56:55 +0900

    Revised

--- DaisyPlus/OpenSSD/Micron_NAND/daisyplus_openssd_micron_4c2w_lpddr4/cosm-plus-sys/cosm-plus-sys.srcs/sources_1/bd/sys_top/hdl/sys_top_wrapper.v
+++ DaisyPlus/OpenSSD/Micron_NAND/daisyplus_openssd_micron_4c2w_lpddr4/cosm-plus-sys/cosm-plus-sys.srcs/sources_1/bd/sys_top/hdl/sys_top_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Tue Jan 17 20:49:12 2023
-//Host        : DESKTOP-KFMNFE2 running 64-bit major release  (build 9200)
+//Date        : Fri Aug 16 17:04:51 2024
+//Host        : DESKTOP-JP2R5GF running 64-bit major release  (build 9200)
 //Command     : generate_target sys_top_wrapper.bd
 //Design      : sys_top_wrapper
 //Purpose     : IP block netlist
